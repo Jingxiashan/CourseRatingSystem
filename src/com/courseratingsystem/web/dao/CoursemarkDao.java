cb463a9f05143728b5fa297045942d38d7231463
kongxiao0532 <kongxao0532@163.com>
2017-07-18 18:19:25 +0800

    delete coursemarkdao

--- src/com/courseratingsystem/web/dao/CoursemarkDao.java
+++
@@ -1,9 +0,0 @@
-package com.courseratingsystem.web.dao;
-
-import com.courseratingsystem.web.domain.Coursemark;
-
-public interface CoursemarkDao {
-	public void add(Coursemark coursemark);
-	public void update(Coursemark coursemark);
-	public void findCoursemarkByCourseId(int courseid);
-}
