b22d75bf5af35656535d99332d789bfa972970ba
killer6 <1511178@mail.nankai.edu.cn>
2017-07-18 15:32:51 +0800

    jiaxing's change2

--- src/com/courseratingsystem/web/service/impl/LogininfoServiceImpl.java
+++ src/com/courseratingsystem/web/service/impl/LogininfoServiceImpl.java
@@ -1,5 +1,38 @@
 package com.courseratingsystem.web.service.impl;
 
-public class LogininfoServiceImpl {
+import com.courseratingsystem.web.dao.LogininfoDao;
+import com.courseratingsystem.web.domain.Logininfo;
+import com.courseratingsystem.web.service.LogininfoService;
 
+public class LogininfoServiceImpl implements LogininfoService{
+	private LogininfoDao logininfoDao;
+	
+	public void setLogininfodao(LogininfoDao logininfoDao) {
+		this.logininfoDao = logininfoDao;
+	}
+
+	@Override
+	public void add(Logininfo logininfo) {
+		logininfoDao.add(logininfo);
+	}
+
+	@Override
+	public void update(Logininfo logininfo) {
+		logininfoDao.update(logininfo);
+	}
+
+	@Override
+	public void delete(Logininfo logininfo) {
+		logininfoDao.delete(logininfo);
+	}
+
+	@Override
+	public Logininfo findLogininfoBycredentialsid(int credentialsid) {
+		return logininfoDao.findLogininfoBycredentialsid(credentialsid);
+	}
+
+	@Override
+	public Logininfo findLogininfoByusername(String username, String password) {
+		return logininfoDao.findLogininfoByusername(username, password);
+	}
 }
